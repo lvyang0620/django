e8f805ef3cef004dac6c0f7ada8a81f8486f496f
lvyang0620 <1043396030@qq.com>
2021-11-21 22:04:57 +0800

    增加分页功能

--- bom/admin.py
+++ bom/admin.py
@@ -8,28 +8,33 @@ class SupplierAdmin(admin.ModelAdmin):
     list_display = ['code','name','contacts_name','contacts_phone','contacts_position','address']
     list_display_links = ['name']
     search_fields = ('code', 'name','contacts_name')
+    list_per_page = 10
 admin.site.register(Supplier,SupplierAdmin)
 #注册Category模型
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['code','name']
     list_display_links = ['code','name']
+    list_per_page = 10
 admin.site.register(Category,CategoryAdmin)
 #注册Material模型
 class MaterialAdmin(admin.ModelAdmin):
     list_display = ['category','code','description','partnumber','supplier']
     list_display_links = ['code','description','partnumber']
     search_fields = ('code','description','partnumber')
+    list_per_page = 5
 admin.site.register(Material,MaterialAdmin)
 #注册Project模型
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['name','description']
     list_display_links = ['name','description']
     search_fields = ('name', 'description')
+    list_per_page = 10
 admin.site.register(Project,ProjectAdmin)
 #注册Bominfo模型
 class BominfoAdmin(admin.ModelAdmin):
     list_display = ['bomname','hw_version','description','project','detail']
     list_display_links = ['bomname']
+    list_per_page = 10
     def detail(self,obj):
         return format_html('<a href="%s">%s</a>' % ('https://www.baidu.com','明细'))
     detail.allow_tags = True
@@ -39,12 +44,14 @@ admin.site.register(Bominfo,BominfoAdmin)
 class EcnAdmin(admin.ModelAdmin):
     list_display = ['content','state','reason','createtime','action','createdby','bominfo']
     list_display_links = ['content']
+    list_per_page = 10
 admin.site.register(Ecn,EcnAdmin)
 #注册Bomlist模型
 class BomlistAdmin(admin.ModelAdmin):
     list_display = ['bominfo','material','description','partnumber','supplier','num','references']
     list_display_links = ['material']
     search_fields = ('bominfo__bomname', 'material__code',)
+    list_per_page = 10
     #显示外键的其他字段的函数
     def description(self,obj):
         return obj.material.description
