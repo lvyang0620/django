11ec6d4d9ca665a95cfd2f91842f6d138ccba0f5
lvyang0620 <1043396030@qq.com>
2021-11-23 12:10:08 +0800

    bomlist中导出功能，添加序号字段，去掉id字段

--- bom/resource.py
+++ bom/resource.py
@@ -6,7 +6,6 @@ from import_export.fields import Field
 from .models import *
 
 class BomlistResource(resources.ModelResource):
-    id = Field(attribute='id', column_name='编号')
     sn = Field(column_name='序号')
     material_code = Field(attribute='material__code', column_name='物料编码')
     material_description = Field(attribute='material__description', column_name='物料描述')
@@ -15,11 +14,16 @@ class BomlistResource(resources.ModelResource):
     num = Field(attribute='num', column_name='数量')
     references = Field(attribute='references', column_name='位号')
 
+    count = 0
     @staticmethod
     def dehydrate_sn(instance: Bomlist):
-        return str(randint(0, 100))
+        BomlistResource.count +=1
+        return BomlistResource.count
+
+    def after_export(queryset, data, *args, **kwargs):
+        BomlistResource.count = 0
 
     class Meta:
         model = Bomlist
-        fields = ('id','sn', 'material_code', 'material_description', 'material_partnumber','material_supplier_name','num','references')
-        export_order = ('id', 'sn','material_code', 'material_description', 'material_partnumber','material_supplier_name','num','references')
+        fields = ('sn', 'material_code', 'material_description', 'material_partnumber','material_supplier_name','num','references')
+        export_order = ('sn','material_code', 'material_description', 'material_partnumber','material_supplier_name','num','references')
