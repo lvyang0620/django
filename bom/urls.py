ac775b2f417a729d9926222f33a13d08d1abe711
lvyang0620 <1043396030@qq.com>
2021-12-01 21:33:41 +0800

    用ListView实现Material,Bominfo,Bomlist,ecn的列表显示

--- bom/urls.py
+++ bom/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
     path('supplierlist/',views.SupplierListView.as_view(),name='supplierlist'),
     path('categorylist/',views.CategoryListView.as_view(),name='categorylist'),
     path('projectlist/',views.ProjectListView.as_view(),name='projectlist'),
+    path('materiallist/',views.MaterialtListView.as_view(),name='materiallist'),
+    path('bominfolist/',views.BominfoListView.as_view(),name='bominfolist'),
+    path('bomlistlist/',views.BomlistListView.as_view(),name='bomlistlist'),
+    path('ecnlist/',views.EcnListView.as_view(),name='ecnlist'),
     path('specific_bom_detail/<str:bomname>/', bominfo_to_bomlist_views.specific_bom_detail,name='specific_bom_detail'),
 
 ]
--- bom/views.py
+++ bom/views.py
@@ -32,6 +32,42 @@ class ProjectListView(ListView):
     #每页显示5条
     paginate_by = 5
 
+# 用Listview实现Material列表显示
+class MaterialtListView(ListView):
+    #查询的模型
+    model = Material
+    #查询返回结果集，优先级最好，设置以后model失效
+    #queryset =
+    #每页显示5条
+    paginate_by = 10
+
+# 用Listview实现Bominfo列表显示
+class BominfoListView(ListView):
+    #查询的模型
+    model = Bominfo
+    #查询返回结果集，优先级最好，设置以后model失效
+    #queryset =
+    #每页显示5条
+    paginate_by = 10
+
+# 用Listview实现Bomlist列表显示
+class BomlistListView(ListView):
+    #查询的模型
+    model = Bomlist
+    #查询返回结果集，优先级最好，设置以后model失效
+    #queryset =
+    #每页显示5条
+    paginate_by = 10
+
+# 用Listview实现# 用Listview实现Ecn列表显示列表显示
+class EcnListView(ListView):
+    #查询的模型
+    model = Ecn
+    #查询返回结果集，优先级最好，设置以后model失效
+    #queryset =
+    #每页显示5条
+    paginate_by = 10
+
 def index(request):
     #return HttpResponse('测试index请求，id={}'.format(id))
     return render(request,'index.html')
